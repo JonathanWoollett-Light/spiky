d93fe852db6467236ef5f6f4cbd28f0b3b3096b0
Jonathan <jonathanwoollettlight@proton.me>
2025-06-19 03:40:45 +0100

    starting test backprop

--- tests/test_forward.py
+++ tests/test_forward.py
@@ -7,9 +7,9 @@ DECAY: float32 = float32(0.8)
 THRESHOLD: float32 = float32(1.0)
 INPUT: int = 28 * 28
 BATCH: int = 10_000
+LEARNING_RATE: float32 = float32(0.02)
 
-
-def test():
+def test_net():
     neuron = sn.LIF(DECAY, THRESHOLD)
     net = sn.FeedForwardNetwork(
         BATCH,
@@ -18,3 +18,16 @@ def test():
     )
     net.forward(cp.zeros((BATCH, INPUT), float32))  # type: ignore
     assert False
+
+def test_bptt():
+    TIMESTEPS = 10
+    neuron = sn.LIF(DECAY, THRESHOLD)
+    net = sn.FeedForwardNetwork(
+        BATCH,
+        INPUT,
+        [(sn.Linear(784), neuron), (sn.Linear(800), neuron), (sn.Linear(10), neuron)],
+    )
+    bptt = sn.BackpropagationThroughTime(net)
+    bptt.forward([cp.zeros((BATCH, INPUT), float32) for _ in range(TIMESTEPS)])  # type: ignore
+    bptt.backward([cp.zeros((BATCH, INPUT), float32) for _ in range(TIMESTEPS)]) # type: ignore
+    bptt.update(LEARNING_RATE)
